3529e46d412f985771a7229ac9971ab97620b025
Pasya Putra <abangadiksantoso869@gmail.com>
2024-03-30 15:06:05 +0700

    Upload Tugas

---
+++ home.php
@@ -0,0 +1,15 @@
+    <div class="card">
+         <div class="card-body">
+            <p>
+                <img src="gambar/WEB GAME.jpg" class="rounded float-end" alt="WebDev:" width="195px" height="195px">
+                <b><i>Selamat Datang, Pengguna!</i></b>
+                <br><b>Web Programming</b> atau <i>Pemrograman Web</i> merupakan istilah yang 
+                erat kaitannya dengan internet dan website. Memang benar, karena pemrograman web 
+                merupakan suatu proses pembuatan website untuk keperluan internet.<br>
+                <br>
+                <b>Student Day Web Programming</b> Merupakan salah satu kelas dalam program student day, Student day web programming merupakan kelas 
+                yang mempelajari tentang pemogramman web. Web programming mebuat instruksi-instruksi untuk menciptakan program/aplikasi berbasis internet yang 
+                dapat diakses melalui browser. Contoh dari pemrograman web itu sendiri yakni: gmail, facebook, tumblr, dll.
+            </p>
+        </div>
+    </div>
